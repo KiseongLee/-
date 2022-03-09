418164291e0cc5803f810ac4bd966db1014723dc
Kiseong Lee <73596016+KiseongLee@users.noreply.github.com>
2022-03-09 18:03:41 +0900

    Update 1.py

--- DFS_BFS_/1.py
+++ DFS_BFS_/1.py
@@ -81,3 +81,18 @@ def factorial_recursive(n):
 
 print(factorial_recursive(5))
 '''
+
+#유클리드 호제법
+
+# 두 자연수 A, B에 대하여(A>B) A를 B로 나눈 나머지를 R이라고 하자
+# 이 때 A와 B의 최대 공약수는 B와 R의 최대 공약수와 같다.
+
+# 유클리드 호제법 재귀 함수로 작성
+
+def gcd(a, b): 
+    if a%b == 0:
+        return b
+    else:
+        return gcd(b, a%b)
+
+print(gcd(192, 162)) 
